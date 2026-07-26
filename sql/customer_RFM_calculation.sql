e24be0539f5d61fb28c6825baa084f3e06f76375
Yashaswini Dara <145293963+Yd1604@users.noreply.github.com>
2026-07-26 21:44:16 +0100

    customer_RFM_calculation.sql

---
+++ sql/customer_RFM_calculation.sql
@@ -0,0 +1,146 @@
+-- 2. CUSTOMER RFM CALCULATION
+
+
+-- RFM stands for:
+-- R = Recency  : Number of days since the customer's last purchase
+-- F = Frequency: Number of unique orders placed by the customer
+-- M = Monetary : Total amount spent by the customer
+
+
+
+--  CHECK TRANSACTION DATE RANGE
+
+
+-- Check the first and last purchase dates in the dataset.
+
+SELECT
+    MIN(DATE(InvoiceDate)) AS First_Purchase_Date,
+    MAX(DATE(InvoiceDate)) AS Last_Purchase_Date
+FROM
+    `segmentation-project-503000.customer_segmentation.cleaned_online_retail`;
+
+
+--  CALCULATE RFM VALUES
+
+
+
+-- Recency:
+-- Calculates the number of days between the customer's
+-- most recent purchase and the analysis date.
+--
+-- Frequency:
+-- Counts the number of unique invoices/orders placed
+-- by each customer.
+--
+-- Monetary:
+-- Calculates the total amount spent by each customer.
+
+SELECT
+    CustomerID,
+
+    DATE_DIFF(
+        DATE '2011-12-10',
+        MAX(DATE(InvoiceDate)),
+        DAY
+    ) AS Recency,
+
+    COUNT(DISTINCT InvoiceNo) AS Frequency,
+
+    ROUND(
+        SUM(TotalPrice),
+        2
+    ) AS Monetary
+
+FROM
+    `segmentation-project-503000.customer_segmentation.cleaned_online_retail`
+
+GROUP BY
+    CustomerID
+
+ORDER BY
+    Monetary DESC;
+
+
+
+--  VALIDATE RFM RESULTS
+
+
+-- Check the number of customers included in the RFM analysis.
+
+WITH rfm_values AS (
+
+    SELECT
+        CustomerID,
+
+        DATE_DIFF(
+            DATE '2011-12-10',
+            MAX(DATE(InvoiceDate)),
+            DAY
+        ) AS Recency,
+
+        COUNT(DISTINCT InvoiceNo) AS Frequency,
+
+        ROUND(
+            SUM(TotalPrice),
+            2
+        ) AS Monetary
+
+    FROM
+        `segmentation-project-503000.customer_segmentation.cleaned_online_retail`
+
+    GROUP BY
+        CustomerID
+)
+
+SELECT
+    COUNT(*) AS Total_Customers,
+    ROUND(AVG(Recency), 2) AS Average_Recency,
+    ROUND(AVG(Frequency), 2) AS Average_Frequency,
+    ROUND(AVG(Monetary), 2) AS Average_Monetary_Value
+
+FROM
+    rfm_values;
+
+
+
+--  VIEW TOP CUSTOMERS BY MONETARY VALUE
+
+
+WITH rfm_values AS (
+
+    SELECT
+        CustomerID,
+
+        DATE_DIFF(
+            DATE '2011-12-10',
+            MAX(DATE(InvoiceDate)),
+            DAY
+        ) AS Recency,
+
+        COUNT(DISTINCT InvoiceNo) AS Frequency,
+
+        ROUND(
+            SUM(TotalPrice),
+            2
+        ) AS Monetary
+
+    FROM
+        `segmentation-project-503000.customer_segmentation.cleaned_online_retail`
+
+    GROUP BY
+        CustomerID
+)
+
+SELECT
+    CustomerID,
+    Recency,
+    Frequency,
+    Monetary
+
+FROM
+    rfm_values
+
+ORDER BY
+    Monetary DESC
+
+LIMIT 10;
